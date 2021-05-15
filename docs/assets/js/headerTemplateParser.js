0a57f77c715785008e5ec3bce3d108573ccb6240
Glaas <s.decuyper314@gmail.com>
2021-05-15 20:30:15 +0200

    A lot of small adjustments

--- docs/assets/js/headerTemplateParser.js
+++ docs/assets/js/headerTemplateParser.js
@@ -2,11 +2,10 @@ const tempolate = `
 <header id="header">
 <a class="logo" href="index.html">Home</a>
 <a class="logo" href="wip.html">Portfolio</a>
-<a class="logo" href="wip.html">Games</a>
+<a class="logo" href="https://glaas.itch.io">Games</a>
 <a class="logo" href="wip.html">Art</a>
 <a class="logo" href="neatstuff.html">Neat stuff I made</a>
 <a class="logo" href="aboutme.html">About me / Contact</a>
-<a class="logo" href="testLab.html">teslab</a>
 
 
 </header>
@@ -18,7 +17,6 @@ function showHeader() {
     ht.innerHTML = tempolate;
 
     document.body.prepend(ht);
-
 
 
 
